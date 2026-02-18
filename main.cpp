1097ac3833828a90aafefec7eeb5467fa16f5e22
U-BSD\455929 <455929@SHS-BYBF6X3.beaverton.k12.or.us>
2026-02-18 12:59:36 -0800

    read txt file

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 #include <cstring>
-
+#include <fstream>
 using namespace std;
 
 
 bool query();
-int sort
+int sort(int num, int array[]);//do i make int array[100]?
 
 
 int main(){
+  int array[100];
+  int num;
   query();
+  if (query){
+    int count;
+    fstream f("nums.txt");
+    while(count < 100 && f >> array[count]){
+      
+      count++;
+    }
+    for(int i=1;i<100;i++){
+      
+      cout << array[i]<<endl;
+    }
+  }
   
   
   return 0;
