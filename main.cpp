3038c732067fdf6f70f6f69d86dbc7a89cf8dda2
U-BSD\455929 <455929@SHS-BYBF6X3.beaverton.k12.or.us>
2026-02-26 13:35:07 -0800

    allow person to input more than 1 num

--- main.cpp
+++ main.cpp
@@ -6,53 +6,72 @@ using namespace std;
 
 
 struct Sort{
-  int firstSpot(int count, ifstream& f, int array[]){//reconsider returntype
+  void firstSpot(int count, ifstream& f, int array[]){//reconsider returntype
     f >> array[count];
     while(count != 1 && needSwap(array, count) == true){
-      count = swap(array, count); //
+      //cout << "hmm";
+      count = swap(count, array); //
     }
-    return array[100];
   }
+  
   bool needSwap(int array[], int count){
     //check if child is bigger than parent
     bool tf = false;
     if (array[count] > array[count/2]){
-      cout << "Parent: " << array[count/2] << " Child: " << array[count] << endl;
+      //cout << "Parent: " << array[count/2] << " Child: " << array[count] << endl;
       tf = true;
     }
     return tf;
   }
-  int swap(){//return new index
+  int swap(int count, int array[]){//return new index
     //swap parent and child
-    return 0;
+    int tempvar=array[count/2];
+    array[count/2] = array[count];
+    array[count] = tempvar;
+    return (count/2);
   }
 };
 
 bool query();
-
+void printTree(int array[], int lastIndex, int curIndex, int depth){
+  if((curIndex*2)+1 < lastIndex){
+    printTree(array, lastIndex, (curIndex*2)+1, depth+1);
+  }
+  for(int i=0; i<depth;i++){
+    cout << "\t";
+  }
+  cout << array[curIndex]<< endl;
+  if((curIndex*2) < lastIndex){
+    printTree(array, lastIndex, (curIndex*2), depth+1);
+  }
+  
+}
 
 int main(){
   Sort s;
   int array[100];
-  int num;
   query();
+  int count = 1;
+  int num;
   if (query){
-    int count = 1;
     ifstream f("nums.txt");
     while(count < 101 && f){
       s.firstSpot(count, f, array);//[100]);
       //f >> array[count];//this is how you insert the num
       count++;
     }
-    for(int i=1;i<101;i++){
-      
-      cout << array[i]<<endl;
+  } else{
+    
+    cout << "What number would you like to add? ";
+    cin >> num;
+    array[count] = num;
+    count++;
     }
-  }
-  
-  
+
+  printTree(array, count, 1, 0);
   return 0;
 }
+
 
 bool query(){
   char ask;
