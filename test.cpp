82d396880668bc1666033e519a0a396acda69e41
julia <455929@bsd48.org>
2026-02-28 00:38:15 +0000

    yay!

--- test.cpp
+++ test.cpp
@@ -29,66 +29,62 @@ struct Sort{
     return (count/2);
   }
 
-	
+  //deletes the biggest thing/head
   void deleteLargest(int array[], int &count){
-    //cout << "First: " <<endl;
-    //printTree(array, count+1, 1,0);
     int index=1;//where the removed  thing is
     cout << array[1] << endl;
-    //bool stillGoing = true;
-    array[index] = array[count];
-    //cout << "2nd: " << endl;
-    //printTree(array, count+1, 1,0);
-    while (array[index] < array[index*2] || array[index] < array[index*2+1]){
-      if (array[index*2] < array[index*2+1]){ //if right is larger than left
-	index = (swap(index*2+1, array))*2+1;
-      } else if(array[index*2] > array[index*2+1]){ // if left is bigger than right
-	index = 2*swap(index*2, array);
+    array[index] = array[count];//replace the head with the last thing in the array
+    while (index*2 <= count) {//so long as it has at least 1 child...
+      if(array[index] < array[index*2] || array[index] < array[index*2+1]){//and the child is bigger than it...
+	if (array[index*2] < array[index*2+1]){ //and if right is larger than left
+	  index = (swap(index*2+1, array))*2+1; //swap parent and child
+	} else if(array[index*2] > array[index*2+1]){ // if left is bigger than right
+	  index = 2*swap(index*2, array);//swap parent and child
+	}else{
+	  break; // safety net
+	}
       }else{
-	cout<< "Party! " << array[index*2+1];
 	break;
       }
     }
-    //cout<< "third: " << endl;
-    array[count] = 0;
-    //printTree(array, count+1, 1,0);
-    count--;
+    array[count] = 0; //get rid of last thing in array (moved to head)
+    count--;//get rid of last thing
   }
 };
 
 char query();
-void printTree(int array[], int lastIndex, int curIndex, int depth){
-  if((curIndex*2)+1 < lastIndex){
-    printTree(array, lastIndex, (curIndex*2)+1, depth+1);
+void printTree(int array[], int lastIndex, int curIndex, int depth){//PRINT!!!
+  if((curIndex*2)+1 < lastIndex){//as long as this index isn't at the end (right child)
+    printTree(array, lastIndex, (curIndex*2)+1, depth+1); //recurssive
   }
-  for(int i=0; i<depth;i++){
+  for(int i=0; i<depth;i++){ // make spaces
     cout << "\t";
   }
-  cout << array[curIndex]<< endl;
-  if((curIndex*2) < lastIndex){
-    printTree(array, lastIndex, (curIndex*2), depth+1);
+  cout << array[curIndex]<< endl; //print child
+  if((curIndex*2) < lastIndex){ // as long as index isn't at end (wrong child)
+    printTree(array, lastIndex, (curIndex*2), depth+1); //recurssive
   }
   
 }
 
 int main(){
+  //rules
    cout << "Type 'f' to input file, 'a' to manually add number, 'd' to delete la\
 rgest number, 'r' to remove all, 'p' to print, or 'q' to quit. " << endl << endl;
 
   Sort s;
-  int array[100];
+  int array[100]; //he said max is 100 ints
   int count = 0;
-  char ask = query();
+  char ask = query(); //print, quit, delete all, delete head,add, add from text file
   int num;
   while(ask != 'q'){
-    if (ask == 'f'){
+    if (ask == 'f'){ //add from text file
       ifstream f("text.txt");
       while(count < 101 && f>> num){
-	//cout << num << " ";
 	count++;
 	s.firstSpot(count, num, array);
       }
-    } else if(ask == 'a'){
+    } else if(ask == 'a'){//manual ask, keeps on going till person stops it
       while(num != -1){
 	cout << "What number would you like to add? (type '-1' to quit) ";
 	cin >> num;
@@ -97,15 +93,13 @@ rgest number, 'r' to remove all, 'p' to print, or 'q' to quit. " << endl << endl
 	  s.firstSpot(count, num, array);
 	}
       }
-      num = 0;      
-    } else if(ask == 'd'){
+      num = 0; //resets so doesn't start at -1
+    } else if(ask == 'd'){//delete head
       s.deleteLargest(array, count);
-      //count--;
-    } else if(ask == 'r'){
+    } else if(ask == 'r'){//delete for all things
       cout << "now remove all" << endl;
-      while(count >= 1){
+      while(count >= 1){//print
 	s.deleteLargest(array,count);
-	//count--;
       }
     } else if(ask == 'p'){
       printTree(array, count+1, 1, 0);
@@ -113,6 +107,7 @@ rgest number, 'r' to remove all, 'p' to print, or 'q' to quit. " << endl << endl
 
     ask = query();
   }
+  cout << "I really wanted to do a for loop printing the same thing ~ 1 billion times so ud think theres an infanite loop, but I didn't. Your welcome!";
   return 0;
 }
 
